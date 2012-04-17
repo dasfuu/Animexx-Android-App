35c30954ea75ce67c3e9efbded38ceb7f7886862
meisterfuu <info@meister-fuu.de>
2012-04-17 22:40:48 +0200

    v1.4.14

--- src/de/meisterfuu/animexx/Constants.java
+++ src/de/meisterfuu/animexx/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
 
 	// Other
 	public static final String LOADING = "Daten werden abgerufen...";
-	public static final String VERSION = "v1.4.13";
+	public static final String VERSION = "v1.4.14";
 
 	// SQLLite
 	public final static String DB_NAME = "Animaxx";
