ffe23d4e053e769b2dd6dbb671f5d123c5501ab6
meisterfuu <info@meister-fuu.de>
2012-06-26 23:28:57 +0200

    Feedbackbutton => About

--- src/de/meisterfuu/animexx/about.java
+++ src/de/meisterfuu/animexx/about.java
@@ -1,8 +1,14 @@
 package de.meisterfuu.animexx;
 
+import de.meisterfuu.animexx.other.Feedback;
+import de.meisterfuu.animexx.profil.UserPopUp;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class about extends Activity {
 	@Override
@@ -10,6 +16,12 @@ public class about extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
 		WebView htm = (WebView) findViewById(R.id.webView1);
+		Button bt = (Button) findViewById(R.id.btfeedback);
+		bt.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent().setClass(getApplicationContext(), Feedback.class));
+			}
+		});
 		htm.loadUrl("http://animexx.meister-fuu.de/about.htm");
 	}
 
