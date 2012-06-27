30a3aafbb6036392edcc8f53766e4d1f90c0e244
meisterfuu <info@meister-fuu.de>
2012-06-27 17:08:28 +0200

    finish() Menu onBackPressed()

--- src/de/meisterfuu/animexx/Menu.java
+++ src/de/meisterfuu/animexx/Menu.java
@@ -106,6 +106,7 @@ public class Menu extends Activity {
 		startMain.addCategory(Intent.CATEGORY_HOME);
 		startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		startActivity(startMain);
+		finish();
 		return;
 	}
 	
