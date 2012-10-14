47e0ec9cf747f8ef88786c681800e6ddfee0039a
meisterfuu <info@meister-fuu.de>
2012-10-14 13:33:41 +0200

    New Picture Upload

--- src/de/meisterfuu/animexx/other/SharePictureENS.java
+++ src/de/meisterfuu/animexx/other/SharePictureENS.java
@@ -1,25 +1,18 @@
 package de.meisterfuu.animexx.other;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.CoreProtocolPNames;
-import org.json.JSONObject;
 
-import de.meisterfuu.animexx.Constants;
 import de.meisterfuu.animexx.Helper;
-import de.meisterfuu.animexx.KEYS;
+import de.meisterfuu.animexx.Request;
 import de.meisterfuu.animexx.ENS.ENSAnswer;
 
 import android.app.Activity;
@@ -27,32 +20,31 @@ import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
 
-public class SharePictureENS extends Activity{
+public class SharePictureENS extends Activity {
+
+	ProgressDialog dialog;
+
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Helper.isLoggedIn(this);
 
 		Intent intent = getIntent();
 		Bundle extras = intent.getExtras();
 		String action = intent.getAction();
+		dialog = new ProgressDialog(this);
 
 		// if this is from the share menu
-		if (Intent.ACTION_SEND.equals(action))
-		{
-			if (extras.containsKey(Intent.EXTRA_STREAM))
-			{
-				try
-				{
+		if (Intent.ACTION_SEND.equals(action)) {
+			if (extras.containsKey(Intent.EXTRA_STREAM)) {
+				try {
 					// Get resource path from intent call
 					Uri uri = (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
 
@@ -64,108 +56,69 @@ public class SharePictureENS extends Activity{
 					byte[] data = getBytesFromFile(is);
 
 					// base 64 encode for text transmission (HTTP)
-					//byte[] encoded_data = Base64.encodeBase64(data);
-					//String data_string = new String(encoded_data);
-					String data_string =  Base64.encodeToString(data, Base64.DEFAULT);
+					// byte[] encoded_data = Base64.encodeBase64(data);
+					// String data_string = new String(encoded_data);
+					String data_string = Base64.encodeToString(data, Base64.DEFAULT);
 
-					//SendRequest(data_string);
-					new SendRequest().execute(data_string);
+					// SendRequest(data_string);
+					final HttpPost request = new HttpPost("https://ws.animexx.de/json/mitglieder/files/upload/?api=2");
+
+					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+					nameValuePairs.add(new BasicNameValuePair("filename", "/Android/" + uri.getLastPathSegment()));
+					nameValuePairs.add(new BasicNameValuePair("data", data_string));
+					nameValuePairs.add(new BasicNameValuePair("type", "base64"));
+					request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+					dialog.setMessage("Uploading photo...");
+					dialog.show();
+
+					final SharePictureENS temp = this;
+
+					new Thread(new Runnable() {
+
+						public void run() {
+							
+							try {
+								Request.SignSend(request);
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+
+
+							temp.runOnUiThread(new Runnable() {
+
+								public void run() {
+									dialog.dismiss();
+									Toast.makeText(temp, "Hochgeladen!", Toast.LENGTH_LONG).show();
+
+									Bundle bundle2 = new Bundle();
+									bundle2.putString("msg", "Ich habe etwas interessantes für dich bei Imgur hochgeladen: \n" + "URL");
+									Intent newIntent = new Intent(getApplicationContext(), ENSAnswer.class);
+									newIntent.putExtras(bundle2);
+									startActivity(newIntent);
+								}
+
+							});
+						}
+
+					}).start();
+					
+
+
 
 					return;
-				} catch (Exception e)
-				{
-					Log.e(this.getClass().getName(), e.toString());
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 
-			} else if (extras.containsKey(Intent.EXTRA_TEXT))
-			{
+			} else if (extras.containsKey(Intent.EXTRA_TEXT)) {
 				return;
 			}
 		}
 
 	}
 
-	private class SendRequest extends AsyncTask<String, Integer, String[]>
-	// private void SendRequest(String data_string)
-	{
-		private final ProgressDialog dialog = new ProgressDialog(SharePictureENS.this);
-
-		protected void onPreExecute() {
-	         this.dialog.setMessage("Uploading photo...");
-	         this.dialog.show();
-	      }
-
-		@Override
-		protected String[] doInBackground(String... data_string) {
-			String result[] = new String[2];
-
-			try {
-				String datai = data_string[0];
-
-				HttpPost request = new HttpPost("http://api.imgur.com/2/upload.json");
-
-				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-				nameValuePairs.add(new BasicNameValuePair("key", KEYS.IMGUR));
-				nameValuePairs.add(new BasicNameValuePair("image", datai));
-				nameValuePairs.add(new BasicNameValuePair("type", "base64"));
-				request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-				DefaultHttpClient httpclient = new DefaultHttpClient();
-				httpclient.getParams().setParameter(CoreProtocolPNames.USER_AGENT,
-						"Android App " + Constants.VERSION);
-				HttpResponse response = httpclient.execute(request);
-				Log.i("Animexx", "Statusline : " + response.getStatusLine());
-				InputStream data = response.getEntity().getContent();
-				BufferedReader bufferedReader = new BufferedReader(
-						new InputStreamReader(data));
-				String responeLine;
-				StringBuilder responseBuilder = new StringBuilder();
-				while ((responeLine = bufferedReader.readLine()) != null) {
-					responseBuilder.append(responeLine);
-				}
-				Log.i("Animexx", "Response : " + responseBuilder.toString());
-				String s = responseBuilder.toString();
-				JSONObject a = new JSONObject(s);
-				result[0] = "Bild erfolgreich hochgeladen!";
-				result[1] = a.getJSONObject("upload").getJSONObject("links").getString("imgur_page");
-			} catch (Exception e) {
-				e.printStackTrace();
-				result[0] = "Hochladen gescheitert!";
-				result[1] = "Hochladen gescheitert!";
-			}
-
-			return result;
-		}
-
-		@Override
-		protected void onProgressUpdate(Integer... values) {
-			super.onProgressUpdate(values);
-			// Update percentage
-		}
-
-		@Override
-		protected void onPostExecute(String[] result) {
-			super.onPostExecute(result);
-
-			this.dialog.dismiss();
-			Toast.makeText(SharePictureENS.this, result[0],
-					Toast.LENGTH_LONG).show();
-			if(result[0].equalsIgnoreCase("Bild erfolgreich hochgeladen!")){
-
-				Bundle bundle2 = new Bundle();
-				bundle2.putString("msg", "Ich habe etwas interessantes für dich bei Imgur hochgeladen: \n"+result[1]);
-				Intent newIntent = new Intent(getApplicationContext(),
-						ENSAnswer.class);
-				newIntent.putExtras(bundle2);
-				startActivity(newIntent);
-
-				SharePictureENS.this.finish();
-			} else {
-				SharePictureENS.this.finish();
-			}
-		}
-
-	}
 
 	public static byte[] getBytesFromFile(InputStream is) {
 		try {
