2350cc0402ad3803614696200d1b566c71a09096
meisterfuu <info@meister-fuu.de>
2013-02-23 14:02:32 +0100

    Fixed ENS Parse Error if An/VonUser is Null("Abgemeldet")

--- src/de/meisterfuu/animexx/ENS/ENSObject.java
+++ src/de/meisterfuu/animexx/ENS/ENSObject.java
@@ -73,16 +73,20 @@ public class ENSObject implements Comparable<Object> {
 			this.setTime(JSON.getString("datum_server"));
 			if (JSON.has("text_html")) this.setText(JSON.getString("text_html"));
 			UserObject von = new UserObject();
-			if (JSON.has("von")) von.ParseJSON(JSON.getJSONObject("von"));
+			if (!JSON.isNull("von")) von.ParseJSON(JSON.getJSONObject("von"));
 			this.setVon(von);
 
-			if (JSON.has("an"))
-			for (int z = 0; z < JSON.getJSONArray("an").length(); z++) {
+			if (!JSON.isNull("an")) {
+				for (int z = 0; z < JSON.getJSONArray("an").length(); z++) {
+					UserObject an = new UserObject();
+					an.ParseJSON(JSON.getJSONArray("an").getJSONObject(z));
+					this.addAnUser(an);
+				}
+			} else {
 				UserObject an = new UserObject();
-				an.ParseJSON(JSON.getJSONArray("an").getJSONObject(z));
 				this.addAnUser(an);
 			}
-
+			
 			if (JSON.has("an_flags")) {
 				this.setFlags(JSON.getInt("an_flags"));
 			} else {
