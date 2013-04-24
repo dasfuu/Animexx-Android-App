7062bab585755d9a04c0c2631d074127328fdf62
meisterfuu <info@meister-fuu.de>
2013-04-24 16:13:23 +0200

    Dashboard update (Karotaler & unread ENS)

--- src/de/meisterfuu/animexx/overview/OverviewActivity.java
+++ src/de/meisterfuu/animexx/overview/OverviewActivity.java
@@ -1,16 +1,16 @@
 package de.meisterfuu.animexx.overview;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 import de.meisterfuu.animexx.Helper;
 import de.meisterfuu.animexx.R;
+import de.meisterfuu.animexx.ENS.ENS;
 import de.meisterfuu.animexx.Request;
-import de.meisterfuu.animexx.events.EventObject;
-import de.meisterfuu.animexx.events.EventViewDetail;
+import de.meisterfuu.animexx.other.KaroTalerAlert;
 import de.meisterfuu.animexx.other.MultiListAdapter;
 import de.meisterfuu.animexx.other.MultiSherlockListActivity;
 import de.meisterfuu.animexx.other.SlideMenu;
@@ -18,10 +18,13 @@ import de.meisterfuu.animexx.other.SlideMenuHelper;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -29,7 +32,6 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class OverviewActivity extends MultiSherlockListActivity {
 
-	EventObject Event;
 	JSONObject tempDetail;
 
 	Context context = this;
@@ -42,6 +44,10 @@ public class OverviewActivity extends MultiSherlockListActivity {
 
 	private SlideMenu slidemenu;
 	private SlideMenuHelper slidemenuhelper;
+
+	int KaroGuthaben, KaroAbholbar, newENS;
+	boolean karo = false;
+	boolean ENS = false;
 
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -56,7 +62,7 @@ public class OverviewActivity extends MultiSherlockListActivity {
 		slidemenu = slidemenuhelper.getSlideMenu();
 		// setup action bar
 		ActionBar actionBar = getSupportActionBar();
-		actionBar.setTitle("Event");
+		actionBar.setTitle("Dashboard");
 		actionBar.setHomeButtonEnabled(true);
 
 		Request.config = PreferenceManager.getDefaultSharedPreferences(context);
@@ -67,20 +73,19 @@ public class OverviewActivity extends MultiSherlockListActivity {
 		ListView lv = getListView();
 		lv.setDivider(null);
 		lv.setDividerHeight(0);
-		
+
 		ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) lv.getLayoutParams();
-		mlp.setMargins(20, 0, 20, 0);
+		mlp.setMargins(30, 40, 30, 40);
 
 		lv.setOnItemClickListener(new OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				if(Array.get(position).getOnclicklistener() != null)Array.get(position).getOnclicklistener().onClick(view);
+				if (Array.get(position).getOnclicklistener() != null) Array.get(position).getOnclicklistener().onClick(view);
 			}
 
 		});
-		
 
-		//refresh();
+		refresh();
 		populate();
 	}
 
@@ -98,48 +103,8 @@ public class OverviewActivity extends MultiSherlockListActivity {
 
 
 	private void refresh() {
-
-		final OverviewActivity temp = this;
-		Loading.setVisibility(View.VISIBLE);
-
-		new Thread(new Runnable() {
-
-			public void run() {
-				try {
-					final String JSON = Request.makeSecuredReq("https://ws.animexx.de/json/events/event/details/?api=2&id=");
-
-					JSONObject jsonResponse = new JSONObject(JSON);
-					JSONObject data = jsonResponse.getJSONObject("return");
-
-					Event = new EventObject();
-					Event.parseJSON(data);
-					Event.setDetail_json(data.toString());
-					tempDetail = data;
-
-					temp.runOnUiThread(new Runnable() {
-
-						public void run() {
-							Loading.setVisibility(View.GONE);
-							populate();
-						}
-					});
-
-				} catch (Exception e) {
-					e.printStackTrace();
-					temp.runOnUiThread(new Runnable() {
-
-						public void run() {
-							adapter.notifyDataSetChanged();
-							Loading.setVisibility(View.GONE);
-							DoError();
-
-						}
-					});
-				}
-
-			}
-		}).start();
-
+		getKaroTalerStats();
+		getNewENSStats();
 	}
 
 
@@ -150,12 +115,131 @@ public class OverviewActivity extends MultiSherlockListActivity {
 
 	public void populate() {
 
-		this.addText("Eingeloggt als: "+ Request.config.getString("username", "Unbekannt"));
-		
+		this.Array.clear();
+
+		this.addText("Eingeloggt als: " + Request.config.getString("username", "Unbekannt"));
+
+		if (this.karo) {
+			this.addSpacer();
+			if (KaroAbholbar > 0) {
+
+				OnClickListener KatoralerClick = new OnClickListener() {
+
+					public void onClick(View v) {
+						Log.i("xx", "KaroTaler Receiver 2");
+						Bundle bundle = new Bundle();
+						bundle.putInt("abholbar", KaroAbholbar);
+						bundle.putInt("guthaben", KaroGuthaben);
+						Intent newIntent = new Intent(context, KaroTalerAlert.class);
+						newIntent.putExtras(bundle);
+						newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						context.startActivity(newIntent);
+					}
+				};
+
+				this.addText("Du kannst " + KaroAbholbar + " abholen. (Guthaben: " + KaroGuthaben + ")", KatoralerClick);
+			} else {
+				this.addText("Du hast " + KaroGuthaben + " Karotaler.");
+			}
+		}
+
+		if (this.ENS) {
+			this.addSpacer();
+			OnClickListener ENSClick = new OnClickListener() {
+
+				public void onClick(View v) {
+					Intent newIntent = new Intent(context, ENS.class);
+					context.startActivity(newIntent);
+				}
+			};
+			if (newENS > 0) {
+				this.addText("Du hast " + newENS + " ungelesene ENS.", ENSClick);
+			} else {
+				this.addText("Du hast keine ungelesenen ENS.", ENSClick);
+			}
+		}
+
 		adapter.notifyDataSetChanged();
 
+	}
+
+
+	public void getKaroTalerStats() {
+
+		final OverviewActivity temp = this;
+		Loading.setVisibility(View.VISIBLE);
+		new Thread(new Runnable() {
+
+			public void run() {
+				try {
+
+					JSONObject a = new JSONObject(Request.makeSecuredReq("https://ws.animexx.de/json/items/kt_statistik/?api=2"));
+					JSONArray ab = a.getJSONObject("return").getJSONArray("kt_abholbar");
+					if (ab.length() > 0) {
+						KaroAbholbar = ab.getJSONObject(0).getInt("kt");
+					} else {
+						KaroAbholbar = 0;
+					}
+					KaroGuthaben = a.getJSONObject("return").getInt("kt_guthaben");
+
+					temp.runOnUiThread(new Runnable() {
+
+						public void run() {
+							temp.karo = true;
+							temp.populate();
+							Loading.setVisibility(View.GONE);
+						}
+					});
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					temp.runOnUiThread(new Runnable() {
+
+						public void run() {
+							Loading.setVisibility(View.GONE);
+							DoError();
+						}
+					});
+				}
+			}
+		}).start();
 
 	}
-	
+
+
+	public void getNewENSStats() {
+
+		final OverviewActivity temp = this;
+		Loading.setVisibility(View.VISIBLE);
+		new Thread(new Runnable() {
+
+			public void run() {
+				try {
+
+					newENS = Request.GetUnread();
+
+					temp.runOnUiThread(new Runnable() {
+
+						public void run() {
+							temp.ENS = true;
+							temp.populate();
+							Loading.setVisibility(View.GONE);
+						}
+					});
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					temp.runOnUiThread(new Runnable() {
+
+						public void run() {
+							Loading.setVisibility(View.GONE);
+							DoError();
+						}
+					});
+				}
+			}
+		}).start();
+
+	}
 
 }
